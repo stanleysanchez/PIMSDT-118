d9a159d70a76e26ebcdb59172454a94510cfd771
Stanley Reginald F. Sanchez <stanley.reginaldsanchez@avensia.com>
2022-10-10 18:32:30 +0800

    IMCIA-286-IMCIA-308-Set separate useState object per customer in order to maintain it and seamlessly change value on refresh.

--- src/components/customerExtensionQueues.jsx
+++ src/components/customerExtensionQueues.jsx
@@ -1,11 +1,60 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "../css/extensionQueue.css";
 
-const customerExtensionQueues = () => {
-    return ( 
-        <React.Fragment>
-            
-        </React.Fragment>
-     );
-}
- 
-export default customerExtensionQueues;
+const CustomerExtensionQueues = (props) => {
+  return (
+    <React.Fragment>
+      <table class="table table-striped">
+        <thead>
+          <tr class="customerNames">
+            <th>{props.customerName}</th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Extension </th>
+            <th>Queue(Extension Stats) </th>
+            <th>Error(Extension Stats) </th>
+            <th>Running(Extension Stats) </th>
+          </tr>
+          {props.stats.map((s) => (
+            <React.Fragment>
+              <tr>
+                {parseInt(s.errorEventCount) > 10 ? (
+                  <th class="errorThresholdReach">{s.extension}</th>
+                ) : (
+                  <th>{s.extension}</th>
+                )}
+                <td>{s.queuedEventCount}</td>
+                {parseInt(s.errorEventCount) > 10 ? (
+                  <td class="errorThresholdReach">
+                    {s.errorEventCount}
+                    <span> </span>
+                    <a href={s.extentionUrl} target="_blank">
+                      Go to CC
+                    </a>
+                  </td>
+                ) : (
+                  <td>
+                    {s.errorEventCount}
+                    <span> </span>
+                    <a href={s.extentionUrl} target="_blank">
+                      Go to CC
+                    </a>
+                  </td>
+                )}
+                <td>{s.currentSequenceNumber > -1 ? "true" : "false"}</td>
+              </tr>
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+};
+
+export default CustomerExtensionQueues;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.css';
-import ExtensionQueue from './components/extensionQueueDashboard';
-import InRiverRssFeed from './components/inRiverRssFeed';
+import "bootstrap/dist/css/bootstrap.css";
+import ExtensionQueue from "./components/extensionQueueDashboard";
+import InRiverRssFeed from "./components/inRiverRssFeed";
 //import logo from '../public/logo192.png';
 //<img src={logo} width="100" height="50"></img>
-//ReactDOM.render(element, document.getElementById('root'));    {autoRefresh()};   
+//ReactDOM.render(element, document.getElementById('root'));    {autoRefresh()};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 let customerEnvironment = "Test";
 //const customers = ["Megaflis","Indeks Retail A/S","Clas Ohlson AB","Finja AB","Babyworld AB"];
 //const customers = ["Megaflis","Babyworld AB","Megaflis","Babyworld AB","Megaflis","Babyworld AB","Megaflis","Babyworld AB"];
-const customers = ["Ahlens AB","Babyworld AB","Birkenstock GmbH","Clas Ohlson AB","Finja AB","Fisher and Paykel","Hills Ltd","HTS Group","HTS Hans Torgersen","Indeks Retail AS","Megaflis","Menigo Foods", "Moelven Industrier"];
+const customers = [
+  "Ahlens AB",
+  "Babyworld AB",
+  "Birkenstock GmbH",
+  "Clas Ohlson AB",
+  "Finja AB",
+  "Fisher and Paykel",
+  "Hills Ltd",
+  "HTS Group",
+  "HTS Hans Torgersen",
+  "Indeks Retail AS",
+  "Megaflis",
+  "Menigo Foods",
+  "Moelven Industrier",
+];
 const errorPerCustomer = [];
-//const customerErrorsTotal = (stats) => {} 
+//const customerErrorsTotal = (stats) => {}
 root.render(
-    <React.StrictMode>          
-        <div class="row">
-        <div class="col-md-3">
-            
-            <InRiverRssFeed />
-            
-                 </div>       
-                 <div class="col-md-9">   
-            
-            <h5>** EXTENSION QUEUES **</h5>        
-                {customers.map((c) => 
-                    <ExtensionQueue customerName={c} customerEnvironment={customerEnvironment}/>
-                )}                
-            </div>           
-            
-        </div>
-    </React.StrictMode>
+  <React.StrictMode>
+    <div class="row">
+      <div class="col-md-3">
+        <InRiverRssFeed />
+      </div>
+      <div class="col-md-9">
+        <h5>** EXTENSION QUEUES **</h5>
+        <ExtensionQueue customerEnvironment={customerEnvironment} />
+      </div>
+    </div>
+  </React.StrictMode>
 );
